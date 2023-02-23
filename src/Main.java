844710655c486d83a3a8ffa802fed8c9773156b4
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 10:15:34 +0530

    ADDED:Code to find length of line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
+    public static final int X1 = 50;
+    public static final int Y1 = 10;
+    public static final int X2 = 2;
+    public static final int Y2 = 6;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparsion Program");
-
+        double lineLength = 0;
+        lineLength = Math.sqrt(((X1 - X2) * (X1 - X2)) + ((Y1 - Y2) * (Y1 - Y2)));
+        System.out.println("Length of line is:-" + lineLength);
     }
 }
