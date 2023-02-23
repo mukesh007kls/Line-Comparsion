37c37442d6d7dc89878c162b0d111f33a610fbf4
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 11:36:40 +0530

    EDITTED:Code to change integer to double

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparsion Program");
         System.out.println("Starting point is:-(" + X1 + "," + Y1 + ")");
-        int firstLineLength = 0;
-        firstLineLength = (int)Math.sqrt(((X1 - X2) * (X1 - X2)) + ((Y1 - Y2) * (Y1 - Y2)));
-        int secondLineLength=0;
+        double firstLineLength = 0;
+        firstLineLength = Math.sqrt(((X1 - X2) * (X1 - X2)) + ((Y1 - Y2) * (Y1 - Y2)));
+        double secondLineLength=0;
         secondLineLength=(int)Math.sqrt(((X1 - X3) * (X1 - X3)) + ((Y1 - Y3) * (Y1 - Y3)));
-        Integer a=new Integer(firstLineLength);
-        Integer b=new Integer(secondLineLength);
+        Double a=new Double(firstLineLength);
+        Double b=new Double(secondLineLength);
         System.out.println(a.equals(b));
     }
 }
